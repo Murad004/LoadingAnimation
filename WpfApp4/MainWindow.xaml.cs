4edcdf04d1db0f422ac213dbb40315de6001e305
Sadi_er59 <Sadi_er59@itstep.edu.az>
2021-12-01 07:19:13 -0800

    Add project files.

---
+++ WpfApp4/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+       
+
+        private void Btn_Click(object sender, RoutedEventArgs e)
+        {
+            using (Loading loading=new Loading(Simulator))
+            {
+                loading.ShowDialog();
+            }
+        }
+
+        void Simulator()
+        {
+            for (int i = 0; i < 800; i++)
+                Thread.Sleep(5);
+        }
+    }
+}
